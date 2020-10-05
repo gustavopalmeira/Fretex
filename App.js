7727899da5c10ae62469ad9973672b566b331a8d
Gustavo <gustavo.palmeira@tc.df.gov.br>
2020-10-05 10:49:06 -0300

    [ADD]Pagina-Cliente/prestador

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // import { StatusBar } from 'expo-status-bar';
 import React, {Component, useState} from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Modal, KeyboardAvoidingView, } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView, Picker, ScrollView} from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
 import {createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation';
@@ -30,8 +30,7 @@ class Login extends Component {
         <Text style={styles.buttonText} >Entrar</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Cadastro')}
->
+        <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Cadastro')}>
         <Text style={styles.buttonText} >Não possuo cadastro</Text>
         </TouchableOpacity>
           </KeyboardAvoidingView>
@@ -82,8 +81,7 @@ class Cadastro extends Component {
             onChangeText={(senha) => setSenha(senha)}
             placeholder="Senha"
         />
-        <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Login')}
->
+        <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Login')}>
         <Text style={styles.buttonText}>Salvar</Text>
         </TouchableOpacity>
         </KeyboardAvoidingView>
@@ -106,7 +104,7 @@ class Perfil extends Component {
         <Text style={styles.buttonText}>Prestador de serviços</Text>
         </TouchableOpacity>
         
-        <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Perfil')}>
+        <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Cliente')}>
         <Text style={styles.buttonText}>Cliente</Text>
         </TouchableOpacity>
       </View>
@@ -133,7 +131,7 @@ class CadastroVeiculo extends Component{
           placeholder="Exemplo: AAA-1234"
         />
 
-        <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Login')}>
+        <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Prestador')}>
           <Text style={styles.buttonText}>Salvar</Text>
         </TouchableOpacity>
         
@@ -144,8 +142,131 @@ class CadastroVeiculo extends Component{
   }
 }
 
+class Frete extends Component {
+  render(){
+    return(
+      <ScrollView style={styles.body}>
+        <Text style={styles.titleFrete}>Solicitar frete</Text>
+        
+        <Text style={styles.titleNome}>Carga a ser transportada:</Text> 
+        <TextInput 
+          style={styles.input}
+          valeu={carga}
+          onChangeText={(carga) => setCArga(carga)}
+          placeholder="Exemplo: geladeira"
+        />
+        
+        <Text style={styles.titleNome}>Endereço de Partida:</Text> 
+        <TextInput 
+          style={styles.input}
+          valeu={cep}
+          onChangeText={(cep) => setCep(cep)}
+          placeholder="CEP"
+        />
 
+        {/* <Picker 
+            style={styles.picker}
+            selectedValue={uf}
+            onValueChange={
+                (itemValor, itemIndex) => this.setState({uf:itemValor})
+        }>
+        <Picker.Item label="UF" value=""/>
+        </Picker>
 
+        <Picker 
+            style={styles.picker}
+            selectedValue={cidade}
+            onValueChange={
+                (itemValor, itemIndex) => this.setState({cidade:itemValor})
+        }>
+        <Picker.Item label="Cidade" value=""/>
+        </Picker>
+
+        <TextInput 
+            style={styles.input}
+            valeu={logradouro}
+            onChangeText={(logradouro) => setLogradouro(logradouro)}
+            placeholder="Logradouro"
+          /> */}
+        <TextInput 
+            style={styles.input}
+            valeu={numero}
+            onChangeText={(numero) => setNumero(numero)}
+            placeholder="Número"
+          />
+        <TextInput 
+            style={styles.input}
+            valeu={complemento}
+            onChangeText={(complemento) => setComplemento(complemento)}
+            placeholder="Complemento"
+          />  
+
+        <Text style={styles.titleNome}>Endereço de Entrega:</Text> 
+        <TextInput 
+          style={styles.input}
+          valeu={cepDestino}
+          onChangeText={(cepDestino) => setCepDestino(cepDestino)}
+          placeholder="CEP"
+        />
+
+        {/* <Picker 
+            style={styles.picker}
+            selectedValue={uf}
+            onValueChange={
+                (itemValor, itemIndex) => this.setState({uf:itemValor})
+            }
+        >
+          <Picker.Item label="UF" value=""/>
+        </Picker>
+
+        <Picker 
+            style={styles.picker}
+            selectedValue={cidade}
+            onValueChange={
+                (itemValor, itemIndex) => this.setState({cidade:itemValor})
+            }
+        >
+          <Picker.Item label="Cidade" value=""/>
+        </Picker>
+
+        <TextInput 
+            style={styles.input}
+            valeu={logradouro}
+            onChangeText={(logradouro) => setLogradouro(logradouro)}
+            placeholder="Logradouro"
+          /> */}
+        <TextInput 
+            style={styles.input}
+            valeu={numeroDestino}
+            onChangeText={(numeroDestino) => setNumeroDestino(numeroDestino)}
+            placeholder="Número"
+          />
+        <TextInput 
+            style={styles.input}
+            valeu={complementoDestino}
+            onChangeText={(complementoDestino) => setComplementoDestino(complementoDestino)}
+            placeholder="Complemento"
+          />                       
+      </ScrollView>
+    );
+    const [cep, setCep] = useState('');
+    const [cepDestino, setCepDestino] = useState('');
+    const [numero, setNumero] = useState('');
+    const [numeroDestino, setNumeroDestino] = useState('');
+    const [complemento, setComplemento] = useState('');
+    const [complementoDestino, setComplementoDestino] = useState('');
+  }
+}
+
+class FretePrestador extends Component {
+  render(){
+    return(
+      <ScrollView style={styles.body}>
+        <Text style={styles.titleFrete}>Solicitações de fretes</Text>
+      </ScrollView>
+    );
+  }
+}
 
 /* NAVEGAÇÃO */
 const AppNavigator = createStackNavigator(
@@ -161,6 +282,12 @@ const AppNavigator = createStackNavigator(
       },
       Veiculo: {
         screen: CadastroVeiculo
+      },
+      Cliente: {
+        screen: Frete
+      },
+      Prestador: {
+        screen: FretePrestador
       }
   },
       {
@@ -196,7 +323,7 @@ const styles = StyleSheet.create({
   margin: 10,
   padding: 10,
   color: '#000',
-  fontSize: 20
+  fontSize: 20,
   },
   button: {
   justifyContent: 'center',
@@ -221,5 +348,20 @@ const styles = StyleSheet.create({
   marginTop: 100,
   fontSize: 25,
   color: '#000'
-  }
+  },
+  titleFrete: {
+  textAlign: 'center',
+  marginTop: 15,
+  fontSize: 25,
+  color: '#000'
+  },
+  picker: {
+    width: 350
+  },
+  titleNome: {
+    marginTop: 15,
+    marginLeft: 10,
+    fontSize: 20,
+    },
   });
+
